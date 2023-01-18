c3be745bcd60ec46feb7d0a2aec3344a2ce9274f
Lou Regla <lou.regla@gofullscale.com>
2023-01-18 12:31:19 -0800

    fixed way that cases array is appended

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const getAllLinks = require("./links");
 
 const app = express();
 const PORT = process.env.PORT || 4200;
-let caseDataFromAPI = {};
+const casesDataFromAPI = [];
 
 app.get("/", (request, response) => {
-    const content = caseDataFromAPI.reduce((content, next) => (`${content}\n${templates.caseItem(next)}`), "");
+    const content = casesDataFromAPI.reduce((content, next) => (`${content}\n${templates.caseItem(next)}`), "");
     response.send(templates.base(content));
 });
 
@@ -16,12 +16,12 @@ app.listen(PORT, async () => {
   console.log("Acquiring data from LexMachina API...");
   const cases = await getCasesFirmForDefendant();
   console.log("Done.");
-  caseDataFromAPI = cases
+  casesDataFromAPI.push(...cases
     .sort(sortByFilingDate)
     .map((districtCase) => ({
       ...districtCase,
       links: getAllLinks(districtCase),
       contact: districtCase.defendantAttorney,
-    }));
+    })));
   console.log(`Server started on port ${PORT}`);
 });
